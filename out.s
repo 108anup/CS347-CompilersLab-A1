17e33bb0b366af079da941834d177665b0d2a33d
108anup <108anup@gmail.com>
2018-02-03 17:54:05 +0530

    Add tests

--- out.s
+++ out.s
@@ -1,13 +1,10 @@
-_LOOP0:
     mov t0, a
     mov t1, b
     cmp t0, t1
-    setg t0
+    sete t0
     cmp t0, $0
     jz _LBL0
 
-    mov t0, e
-    mov d, t0
     mov t0, a
     mov t1, b
     cmp t0, t1
@@ -15,28 +12,26 @@ _LOOP0:
     cmp t0, $0
     jz _LBL1
 
-    mov t0, e
-    mov d, t0
-
-_LBL1: 
-    mov t0, e
-    mov f, t0
-    jmp _LOOP0
-
-_LBL0: 
+_LOOP0:
     mov t0, a
     mov t1, b
     cmp t0, t1
-    setl t0
+    sete t0
     cmp t0, $0
     jz _LBL2
 
-    mov t0, e
-    mov d, t0
+    mov t0, d
+    mov c, t0
+    jmp _LOOP0
 
 _LBL2: 
-    mov t0, g
-    mov t1, m
-    cmp t0, t1
-    sete t0
-    mov h, t0
+    mov t0, e
+    mov t1, f
+    sub	t0, t1
+    mov e, t0
+
+_LBL1: 
+    mov t0, n
+    mov m, t0
+
+_LBL0: 
